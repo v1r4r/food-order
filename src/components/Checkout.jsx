364386c6add4ab0d8fd3201217493ff96f4dc194
V1R4R <welenc09@gmail.com>
2025-09-02 20:25:51 +0200

    add food order application

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,111 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formating";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import Input from "./Input";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+import { useActionState } from "react";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const { data, isLoading, error, sendRequest, clearData } = useHttp(
+    "http://localhost:3000/orders",
+    requestConfig
+  );
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleFinish() {
+    userProgressCtx.hideCheckout();
+    cartCtx.clearCart();
+    clearData();
+  }
+
+  function checkoutAction(prevState, fd) {
+    const custumerData = Object.fromEntries(fd.entries());
+
+    sendRequest(
+      JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: custumerData,
+        },
+      })
+    );
+  }
+
+  const [formState, formAction, isSending] = useActionState(
+    checkoutAction,
+    null
+  );
+
+  let actions = (
+    <>
+      <Button type="button" textOnly onClick={handleClose}>
+        Close
+      </Button>
+      <Button>Submit Order</Button>
+    </>
+  );
+
+  if (isSending) {
+    actions = <span>Sending order data...</span>;
+  }
+
+  if (data && !error) {
+    return (
+      <Modal
+        open={userProgressCtx.progress === "checkout"}
+        onClose={handleClose}>
+        <h2>Success!</h2>
+        <p>Your order was submitted successfully.</p>
+        <p>
+          We will get back to you with more details via email within the next
+          few minutes.
+        </p>
+        <p className="modal-actions">
+          <Button onClick={handleFinish}>OK</Button>
+        </p>
+      </Modal>
+    );
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form action={formAction}>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+        <Input label="Full name" type="text" id="name" />
+        <Input label="E-mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="Text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+
+        {error && <Error title="Failed to submit order" message={error} />}
+
+        <p className="modal-actions">{actions}</p>
+      </form>
+    </Modal>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
+
+export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="Logo image" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
+      </nav>
+    </header>
+  );
+}
